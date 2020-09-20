95aea14dc9694ee6835aca53eef280903b14aa63
WinWisely268 <winwisely268@gmail.com>
2020-09-21 02:43:37 +0700

    removes go-gen hack

--- gms-go/generate.go
+++ gms-go/generate.go
@@ -1,3 +1,3 @@
 package main
 
-//go:generate protoc -I./../proto -I. --go_out=paths=source_relative:./../proto ./../proto/chat_service.proto
+//go:generate protoc -I./../proto -I. --go_out=paths=source_relative:./rpc ./../proto/chat_service.proto
